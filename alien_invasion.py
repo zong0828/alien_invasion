d75873c1c9cc71532dd43cce8fb0783ac5b19de3
Zong <zong.xie.udn@gmail.com>
2020-09-13 13:32:51 +0800

    feat. add ship into my game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """ Overall class to manager game assets and behavior """
@@ -15,6 +16,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption(self.settings.game_name)
+        # 這裡的 self 指到的是 AlienInvsion instance
+        self.ship = Ship(self)
 
     def run_game(self):
         """ start the main loop for the game """
@@ -28,6 +31,7 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop
             # pygame 背景色預設為黑色, 此行是幫背景色換顏色
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             # display.flip 會更新螢幕讓 pygame 最新繪製的螢幕畫面顯示, 因此當我們在移動時, 會把舊的畫面隱藏, 新的畫面顯示
---
+++ ship.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Ship:
+    """ Aclass to manage the ship """
+    # ai_game 為 AlienInvasion 的 instance
+    def __init__(self, ai_game):
+        """ init the ship, and set its starting position. """
+        # 取得遊戲視窗大小
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        # 取得圖型大小
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start each new ship at the bottom center of the screen
+        # 指定飛船位置到遊戲螢幕的正中下方
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """ Draw the ship at its current location """
+        # 該 blitme 會根據 self.rect 的位置, 建立飛船到遊戲螢幕上
+        self.screen.blit(self.image, self.rect)
